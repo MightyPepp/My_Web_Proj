c349993454f36867f1253c6192db72ccd09c913d
Aleksandr_Samsonov <samsonovaleksandr1352006@gmail.com>
2025-12-10 09:26:15 +0300

    http/net is working, stack same

--- main.go
+++ main.go
@@ -3,8 +3,16 @@ package main
 import (
 	"fmt"
 	// "My_Web_Proj/stack"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Test")
+	fmt.Println("---------->><<----------")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Yooooo web")
+	})
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting the server", err)
+	}
 }

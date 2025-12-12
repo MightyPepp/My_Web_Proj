3279353c6ca31250bd807758cdab7c13fae68326
Aleksandr_Samsonov <samsonovaleksandr1352006@gmail.com>
2025-12-12 16:07:17 +0300

    Бляяяяяяяяяяяяяяяяяя

--- server/server.go
+++ server/server.go
@@ -1,8 +1,9 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
-	"strings"
+	// "strings"
 )
 
 func StartServer() {
@@ -10,12 +11,13 @@ func StartServer() {
 	r := http.NewServeMux()
 
 	//Хуй пойми в итоге что делает эта строка (либо я тупой?)
-	r.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	// r.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		path := r.URL.Path
-		path = strings.TrimPrefix(path, "/")
-		http.ServeFile(w, r, "/static/" + path + ".html")
+		// path := r.URL.Path
+		// path = strings.TrimPrefix(path, "/")
+		// http.ServeFile(w, r, "/static/" + path + ".html")
+		fmt.Fprintf(w, "Hello blyat'")
 	})
 
 	http.ListenAndServe(":8080", nil)

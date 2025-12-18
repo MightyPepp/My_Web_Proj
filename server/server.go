9537191a493e2f2971c1821f39950cf72544f624
Aleksandr_Samsonov <samsonovaleksandr1352006@gmail.com>
2025-12-18 19:47:59 +0300

    Тупая маршрутизация без мьютекса - работает. Остальное ещё предстоит сделать.

--- server/server.go
+++ server/server.go
@@ -15,12 +15,21 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./static/index.html")
 }
 
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Запрос в about")
+	http.ServeFile(w, r, "./static/about.html")
+}
+
+func anotherHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Запрос в another")
+	http.ServeFile(w, r, "./static/another.html")
+}
+
 func StartServer(port string) {
 
 	http.HandleFunc("/", homeHandler)
-	// http.HandleFunc("/about", aboutHandler)
-	// http.HandleFunc("/api/data", apiDataHandler)
-	// http.HandleFunc("api/users", usersHandler)
+	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/another", anotherHandler)
 
 	log.Printf("Сервер запущен на http://localhost:%s", port)
 	err := http.ListenAndServe(":" + port, nil)
